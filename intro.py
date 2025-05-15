9f3dae967c8d06b124154af44eae323c423063b7
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-15 16:56:06 +0300

    first commit

---
+++ intro.py
@@ -0,0 +1,11 @@
+# print("Billjoy")
+# print("100")
+# print(2 / 100)
+
+
+#calculate area of a rectangle
+print("The length of a rectangle is 20 and the width is 15")
+length = 20
+width = 15
+area = length * width
+print("The area of our Rectangle =",area)
