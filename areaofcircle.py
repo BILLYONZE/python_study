10814601c0c21dd42fb11255d014ff2c98ca0b10
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-16 15:47:02 +0300

    learned Strings

--- areaofcircle.py
+++ areaofcircle.py
@@ -4,7 +4,7 @@
 # r will be our Radius.
 pi=22/7
 r=7
-area=pi*r*r
+area=pi*r**2
 print("The area of a circle with radius",r,"is",area)
 # The area of a circle with radius 7 is 154.0
 # when finding the area of a circle, the answer is always in square units.
---
+++ strings.py
@@ -0,0 +1,20 @@
+first_name="Billjoy"
+second_name = "Mbaluka"
+print (type(first_name))
+# <class 'str'> is a string.
+# Strings are a sequence of characters enclosed in quotes.
+pi=22/7
+r=7
+area=pi*r**2
+print(area,type(area))
+#154.0 <class 'float'>
+#floats are numbers with decimal points.
+# The area of a circle with radius 7 is 154.0
+# hence the area of a circle is a float.
+x=True
+print(type(x))
+# <class 'bool'>
+# Booleans are True or False values.
+p=4
+print(type(p))
+# <class 'int'>
