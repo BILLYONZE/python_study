bedd4471a9d5e49a45ef76689efb7cef6e72afb7
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-16 16:58:06 +0300

    learned Variable Datatypes

--- strings.py
+++ strings.py
@@ -18,3 +18,39 @@ print(type(x))
 p=4
 print(type(p))
 # <class 'int'>
+full_name = first_name + " " + second_name
+print(full_name)
+# Billjoy Mbaluka
+# The + operator is used to concatenate strings.
+x="1000"
+y="2000"
+print(x+y)
+# 10002000
+# The above is a string concatenation.
+x=1000
+y=2000
+print(x+y)
+# 3000
+# The above is an integer addition.
+# The + operator is used to add numbers.
+# x = "1000"
+# y = 2000
+# print(x+y)
+# # this gives a type error.
+
+my_name = "Billjoy"
+print(my_name[0])
+# this is indexing
+
+text="python programmer"
+print(text[0:8])
+print(text[7:17])
+# this is slicing
+# The above will print the characters from index 7 to 16.
+
+# Below are some string methods.
+# The string methods are used to manipulate strings.
+my_text = "hELLO"
+my_text =my_text.capitalize()
+my_text= my_text.count("l")
+print(my_text)
