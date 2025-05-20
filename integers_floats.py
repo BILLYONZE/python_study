345d11f962306b818cda681c9e890c3081439074
BILLYONZE <billjoy.kilai@gmail.com>
2025-05-20 17:00:06 +0300

    Integers and Floats

---
+++ integers_floats.py
@@ -0,0 +1,26 @@
+x=99
+y=5
+# modulus (%) 4
+q = x%y
+print(q)
+# floor(//) 19
+z = x//y
+print(z)
+# exponentiation (**) 970299
+a = x**3
+print(a)
+myvar = 1000
+myvar = str(myvar)
+print(type(myvar))
+# <class 'str'>
+myvar = int(myvar)
+print(type(myvar))
+# <class 'int'>
+myvar = float(myvar)
+print(type(myvar))
+print(myvar)
+# to get 2 inputs from the user and sum them up
+length_A = int(input("Enter the length of A: "))
+length_B = input("Enter the length of B: ")
+length_B = int(length_B)
+print(f"The total length of the values you chose is {length_A + length_B}")
